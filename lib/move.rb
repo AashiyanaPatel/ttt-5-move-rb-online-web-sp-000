9ff53044248f233d501b563f71b745102a99ba6a
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-01-31 03:50:17 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+puts "Welcome to Tic Tac Toe!"
+puts"Where would you like to go?"
+board = ["", "", "X", "", "", "", "","", ""]
